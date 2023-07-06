c4e683021198a0677180033be4c8775c6b7f9438
sujitra-4996-interaction-labs <137374255+sujitra-4996-interaction-labs@users.noreply.github.com>
2023-07-06 22:03:34 +0700

    Delete hypotenuse.py

--- hypotenuse.py
+++
@@ -1,6 +0,0 @@
-import math
-def call_hypotenunse(a,b):
-    print('Sqrt({}^2) + {}^2) is {:.1f}'.format(a,b, (math.sqrt(a**2 + b**2))))
-call_hypotenunse(3.0,4.0)
-call_hypotenunse(3,4)
-call_hypotenunse(3,4.0)
