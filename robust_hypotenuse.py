c89d8ec7ef7d1cf711901a6c1c9b5e433b087890
sujitra-4996-interaction-labs <137374255+sujitra-4996-interaction-labs@users.noreply.github.com>
2023-07-06 22:03:51 +0700

    Delete robust_hypotenuse.py

--- robust_hypotenuse.py
+++
@@ -1,9 +0,0 @@
-import math
-def hypotenuse(a,b):
-    try:
-        return math.sqrt(math.pow(a,2)+math.pow(b,2))
-    except TypeError:
-        return None
-print(f'hypotenuse(3,4)is {hypotenuse(3.0,4.0)}')
-print(f"hypotenuse('3','4') is {hypotenuse('3','4')}")
-print(f"hypotenuse(3,'4.0') is {hypotenuse(3,'4.0')}")
